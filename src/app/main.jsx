c3e42cd59f612628a888bc4d79b6211261bff4bf
Ayman Tahiat Faiyaz <tahiat0967@gmail.com>
2025-08-21 10:58:17 +0600

    Initial commit from Create Next App

---
+++ src/app/main.jsx
@@ -0,0 +1,13 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { player } from "./components/Player";
+
+const scene = new THREE.Scene();
+scene.add(player);
+
+const camera = Camera();
+scene.add(camera);
+
+const renderer = Renderer();
+renderer.render(scene, camera);
